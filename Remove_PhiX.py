9fd4ee3447e1a7b6d584a59664ceaa269c0005b1
AoAoAoLion <wsq199389@gmail.com>
2024-05-07 11:46:23 -0500

    Update Remove_PhiX.py

--- Remove_PhiX.py
+++ Remove_PhiX.py
@@ -1,19 +1,27 @@
 import os
 import argparse
 
-# 设置命令行参数解析
-parser = argparse.ArgumentParser(description="Run bbduk.sh with specified parameters.")
-parser.add_argument("sample_id", required=True, help="Sample ID to process")
-parser.add_argument("--bbmappath", required=True, help="Base path to the BBMap software")
-args = parser.parse_args()
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Run bbduk.sh with specified parameters.")
+    parser.add_argument("sample_id", help="Sample ID to process")
+    parser.add_argument("--bbmappath", required=True, help="Base path to the BBMap software")
+    return parser.parse_args()
 
-bbmappath = args.bbmappath
-SampleID = args.sample_id
+def construct_command(args):
+    bbmappath = args.bbmappath
+    sample_id = args.sample_id
+    return (f"{bbmappath}/bbmap/bbduk.sh -in1={sample_id}_index.fastq.gz in2={sample_id}_index.fastq.gz "
+            f"out1={sample_id}_index.unmapedphix.fastq.gz out2={sample_id}_index.unmapedphix.fastq.gz "
+            f"outm1={sample_id}_index.mappedphix.fastq.gz outm2={sample_id}_index.mappedphix.fastq.gz "
+            f"ref={bbmappath}/bbmap/resources/phix_adapters.fa.gz k=31 hdist=2 stats=stats.txt")
 
-command = f"{bbmappath}/bbmap/bbduk.sh -in1={SampleID}_filter.R1.fastq in2={SampleID}_filter.R2.fastq " \
-          f"out1={SampleID}_filter.unmaped.R1.fastq out2={SampleID}_filter.unmaped.R2.fastq " \
-          f"outm1={SampleID}_filter.mappedphix.R1.fastq outm2={SampleID}_filter.mappedphix.R2.fastq " \
-          f"ref={bbmappath}/bbmap/resources/phix_adapters.fa.gz k=31 hdist=2 stats=stats.txt"
+def run_command(command):
+    os.system(command)
 
-os.system(command)
+def main():
+    args = parse_arguments()
+    command = construct_command(args)
+    run_command(command)
 
+if __name__ == "__main__":
+    main()
