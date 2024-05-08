55679795f55ed6cc26e17bb06e2fcb8460fd4dfe
AoAoAoLion <wsq199389@gmail.com>
2024-05-08 13:08:02 -0500

    update

--- Merge_reads.py
+++ Merge_reads.py
@@ -1,6 +1,9 @@
 import os
 import argparse
 import subprocess
+import gzip
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Run PEAR with specified parameters.")
@@ -10,7 +13,7 @@ def parse_arguments():
 def construct_command(args):
     sample_id = args.sample_id
     return (f"pear -f {sample_id}_R1_001_index_unmapedphix.fastq.gz -r {sample_id}_R2_001_index_unmapedphix.fastq.gz "
-            f"-o {sample_id}_index_mappedphix_merged -j 4")
+            f"-o {sample_id}_index_unmappedphix_merged -j 4")
 
 def gzip_command(args):
     sample_id = args.sample_id
@@ -19,12 +22,46 @@ def gzip_command(args):
 def run_command(command):
     os.system(command)
 
+def merge_unassembled_reads(args):
+    sample_id = args.sample_id
+    forward_file = f"{sample_id}_index_unmappedphix_merged.unassembled.forward.fasta.gz"
+    reverse_file = f"{sample_id}_index_unmappedphix_merged.unassembled.reverse.fasta.gz"
+
+    # Read forward and reverse files
+    with gzip.open(forward_file, "rt") as f_fasta, gzip.open(reverse_file, "rt") as r_fasta:
+        forward_reads = SeqIO.to_dict(SeqIO.parse(f_fasta, "fasta"))
+        reverse_reads = SeqIO.to_dict(SeqIO.parse(r_fasta, "fasta"))
+
+    # Merge reads based on sequence identifier
+    merged_reads = {}
+    for seq_id in forward_reads:
+        if seq_id in reverse_reads:
+            merged_reads[seq_id] = (forward_reads[seq_id].seq, reverse_reads[seq_id].seq)
+
+    return merged_reads
+
+def write_merged_reads(args, merged_reads):
+    sample_id = args.sample_id
+    output_file = f"{sample_id}_index_unmappedphix_merged.unassembled.merged_by_seqid.fasta.gz"
+
+    # Create SeqRecord objects for each merged read
+    records = []
+    for seq_id, sequences in merged_reads.items():
+        record = SeqRecord(sequences[0] + sequences[1], id=seq_id, description="")
+        records.append(record)
+
+    # Write merged reads to a gzipped fasta file
+    with gzip.open(output_file, "wt") as output_fasta:
+        SeqIO.write(records, output_fasta, "fasta")
+
 def main():
     args = parse_arguments()
     command = construct_command(args)
     run_command(command)
     gzip = gzip_command(args)
     run_command(gzip)
+    merged_reads = merge_unassembled_reads(args)
+    write_merged_reads(args, merged_reads)
 
 if __name__ == "__main__":
     main()
